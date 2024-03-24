77304074c5496526e424633a690a01dbd906307c
arhaamwanii <arhamwani765@gmail.com>
2024-03-24 23:44:55 +0530

    bomba clat

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,25 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [AvailablePlaces , setAvailablePlaces] = useState([])
+  const [availablePlaces , setAvailablePlaces] = useState([])
 
+  useEffect(() => { 
   fetch('http://localhost:3000').then((response) => {
     return response.json()
   }).then((resData) => {
-    resData.places
-  })
-  //will return a promise, that will yield different values
-  //we then can use ".then " which will then do something once the value is recieved, like call a function which will automatically receive the value from the promise
-  // as we have defined in the backend -- the ultimate resonse will have data the way we have specified in the backend
-  // keeping fetch inside of the fuctional component -- every time the functional component executes the coed
+    setAvailablePlaces(resData.places)
+  })    
+  } , [])
 
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
   );
 }
+ 
+
+//will return a promise, that will yield different values
+  //we then can use ".then " which will then do something once the value is recieved, like call a function which will automatically receive the value from the promise
+  // as we have defined in the backend -- the ultimate resonse will have data the way we have specified in the backend
+  // keeping fetch inside of the fuctional component -- every time the functional re-renders: everytime
